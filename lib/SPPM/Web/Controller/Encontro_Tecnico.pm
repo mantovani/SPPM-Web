b398ecc601de03fd92052057c3adfdd7c373f5a4
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2010-04-13 17:23:19 -0300

    mais validacoes

--- lib/SPPM/Web/Controller/Encontro_Tecnico.pm
+++ lib/SPPM/Web/Controller/Encontro_Tecnico.pm
@@ -12,28 +12,42 @@ controller SPPM::Web::Controller::Encontro_Tecnico {
     }
 
     final action enjoy (Int $id) as 'inscrever' under base {
-        $ctx->stash->{'id'} = $id;
-        $ctx->stash( template => 'encontrotecnico/inscrever.tt' );
-        $ctx->forward('View::TT');
+        my $check = $ctx->model('Encontros::EncontroTecnico')->find(
+            { id   => $id }
+        );
+        if ($check) {
+            $ctx->stash->{'id'} = $id;
+            $ctx->stash( template => 'encontrotecnico/inscrever.tt' );
+            $ctx->forward('View::TT');
+        } else {
+            $ctx->res->redirect($ctx->uri_for('../','index'));
+        }
     }
 
     final action subscribe as 'inserir' under base {
-        my $infos = $ctx->req->body_params;
-        
-        my $check = $ctx->model('Encontros::Inscrico')->find({ 
-            email       => $infos->{'email'},
-            encontro_id => $infos->{'encontro_id'}
-        });
-
-        if(!$check) {
-            my $insert = $ctx->model('Encontros::Inscrico')->create($infos);
-            $ctx->stash( template => 'encontrotecnico/ok.tt' );
-        } else {
-            $ctx->stash( template => 'encontrotecnico/not_ok.tt' );
+    
+        if ( $ctx->req->method eq 'POST' ) {
+            my $infos = $ctx->req->body_params;                                                        
+                                                                                                       
+            my $check = $ctx->model('Encontros::Inscrico')->find(                                      
+                {   email       => $infos->{'email'},                                                  
+                    encontro_id => $infos->{'encontro_id'}                                             
+                }                                                                                      
+            );                                                                                         
+                                                                                                       
+            if ( !$check ) {                                                                           
+                my $insert
+                    = $ctx->model('Encontros::Inscrico')->create($infos);                              
+                $ctx->stash( template => 'encontrotecnico/ok.tt' );                                    
+            } else {
+                $ctx->stash( template => 'encontrotecnico/not_ok.tt' );                                
+            }                                                                                          
+             $ctx->forward('View::TT');
         }
-
-        $ctx->forward('View::TT');
-    }
+        else {
+            $ctx->res->redirect($ctx->uri_for('../','index'));
+        }
+    } 
 
 }
 
