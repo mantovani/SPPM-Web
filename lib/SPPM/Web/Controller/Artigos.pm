9fd7fbbfce987b818b8bbc7f1f92525eed319733
Daniel de Oliveira Mantovani <daniel.oliveira.mantovani@gmail.com>
2010-08-02 19:31:18 -0300

    de CX para C

--- lib/SPPM/Web/Controller/Artigos.pm
+++ lib/SPPM/Web/Controller/Artigos.pm
@@ -1,60 +1,52 @@
 
-use CatalystX::Declare;
+use Catalyst;
 
-controller SPPM::Web::Controller::Artigos {
+use File::stat;
+use POSIX qw(strftime);
+use DateTime;
+use SPPM::Web::Pod;
 
-    use File::stat;
-    use POSIX qw(strftime);
-    use DateTime;
-    use SPPM::Web::Pod;
+sub artigo : Chained('base') : PathPart('artigo') : Args(2) {
+    my ( $self, $c, $year, $article ) = @_;
+    my $legal_chars = quotemeta('.-_/');
 
-    action base as 'artigo' under '/base';
-
-    final action article (Int $year, Str $article) as '' under base {
-        my $legal_chars = quotemeta('.-_/');
-
-        if ($article =~ /\.\./ || $article =~ /[^\w$legal_chars]/ ) {
-            $ctx->res->redirect('/');
-            $ctx->detach;
-        }
-
-        my $pod_file = $ctx->path_to('root','artigos',$year,"$article.pod");
-            
-        if (! -e $pod_file) {
-            $ctx->res->redirect('/');
-            $ctx->detach;
-        }
-
-        my $mtime = ( stat $pod_file )->mtime;
-        my $cached_pod = $ctx->cache->get("$pod_file $mtime");
-
-        if (!$cached_pod) {
-            my $parser = SPPM::Web::Pod->new(
-                StringMode      => 1,
-                FragmentOnly    => 1,
-                MakeIndex       => 0,
-                TopLinks        => 0,
-            );
-
-            open my $fh, '<:utf8', $pod_file
-                or die "Failed to open $pod_file: $!";
-
-            $parser->parse_from_filehandle($fh);
-            close $fh;
-
-            $cached_pod = $parser->asString;
-            $ctx->cache->set("$pod_file $mtime", $cached_pod, '12h' );
-        }
-
-        $ctx->stash(
-            pod => $cached_pod,
-            template => 'local/artigo_pod.tt' 
-        );
-        
-        $ctx->forward('View::TT');
-
+    if ( $article =~ /\.\./ || $article =~ /[^\w$legal_chars]/ ) {
+        $c->res->redirect('/');
+        $c->detach;
     }
 
+    my $pod_file = $c->path_to( 'root', 'artigos', $year, "$article.pod" );
+
+    if ( !-e $pod_file ) {
+        $c->res->redirect('/');
+        $c->detach;
+    }
+
+    my $mtime      = ( stat $pod_file )->mtime;
+    my $cached_pod = $c->cache->get("$pod_file $mtime");
+
+    if ( !$cached_pod ) {
+        my $parser = SPPM::Web::Pod->new(
+            StringMode   => 1,
+            FragmentOnly => 1,
+            MakeIndex    => 0,
+            TopLinks     => 0,
+        );
+
+        open my $fh, '<:utf8', $pod_file
+          or die "Failed to open $pod_file: $!";
+
+        $parser->parse_from_filehandle($fh);
+        close $fh;
+
+        $cached_pod = $parser->asString;
+        $c->cache->set( "$pod_file $mtime", $cached_pod, '12h' );
+    }
+
+    $c->stash(
+        pod      => $cached_pod,
+        template => 'local/artigo_pod.tt'
+    );
+
+    $c->forward('View::TT');
 }
-
-
