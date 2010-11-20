f608e47dca47bac58c36137683c2abff2cd3fb56
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2010-11-20 13:54:53 -0200

    FS.pm para o Local e o Artigo.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

---
+++ lib/SPPM/FS.pm
@@ -0,0 +1,53 @@
+package SPPM::FS;
+
+use Moose;
+
+has legal_chars => (
+    is => 'ro',
+    isa => 'Str',
+    default => quotemeta('.-_/')
+);
+
+has basedir => (
+    is => 'ro',
+    isa => 'Str',
+    required => 1
+);
+
+after basedir => sub {
+    my $self = shift;
+    my $orig = shift;
+    
+    return if !$orig;
+
+    die "Where is the basedir ?" if ! -d $orig;
+};
+
+has file => (
+    is => 'rw',
+    isa => 'Str',
+);
+
+after file => sub {
+    my $self = shift;
+    my $orig = shift;
+    return if !$orig;
+   
+    die "This file is insane" 
+        if $orig =~ /\.\./ || $orig =~ /[^\w$self->legal_chars]/;
+
+    $self->fullpath(join('/', $self->basedir, $orig));
+
+    die "What file?"
+        if ! -f $self->fullpath;
+
+};    
+    
+has fullpath => (
+    is => 'rw',
+    isa => 'Str',
+    default => ''
+);
+        
+1;
+
